51b443a91981d064aa78c8f7ff920c7214b91b24
TalgatBatyrov <90198155+TalgatBatyrov@users.noreply.github.com>
2022-10-09 12:43:37 +0600

    step-8

---
+++ lib/services/auth/auth_provider.dart
@@ -0,0 +1,9 @@
+import 'package:basic_registration/services/auth/auth_user.dart';
+
+abstract class AuthProvider {
+  AuthUser? get currentUser;
+  Future<AuthUser> logIn({required email, required password});
+  Future<AuthUser> createUser({required email, required password});
+  Future<void> logOut();
+  Future<void> sendEmailVerification();
+}
